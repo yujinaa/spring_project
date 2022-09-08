3dcfc45349625fe4e3f32cd8aa0df60d1e19b7c9
yujinaa <olim920@naver.com>
2022-09-08 17:02:05 +0900

    비밀번호 암호화기능, 변경전값과 변경후값 비교코드

--- Board/src/main/java/com/care/board/member/service/memberServiceImpl.java
+++ Board/src/main/java/com/care/board/member/service/memberServiceImpl.java
@@ -1,6 +1,7 @@
 package com.care.board.member.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.care.board.member.dto.memberDTO;
@@ -9,16 +10,29 @@ import com.care.board.mybatis.member.MemberMapper;
 @Service
 public class memberServiceImpl implements memberService{
 	@Autowired MemberMapper mapper; //빈주입
+	BCryptPasswordEncoder psencoder; //비번 암호화,변경
+	
+	public memberServiceImpl(){//객체 생성후 생성자 만들기
+		psencoder = new BCryptPasswordEncoder();
+	}
 	public int userCheck(String userId, String userPwd) {
 		memberDTO dto = mapper.userCheck(userId);
 		if(dto != null) {//데이터 있음
-			if(userPwd.equals(dto.getUserPwd())) {
+			//if(userPwd.equals(dto.getUserPwd())) {
+			if(psencoder.matches(userPwd, dto.getUserPwd())) { //암호화된 비번을 원래비번과 비교하기(앞은 사용자입력값, 뒤는 암호화된값)
 				return 0; //로그인 성공
 			}
 		}
 			return 1; //실패
 		}
 	public int signUp(memberDTO dto) {
+		//비번 암호화 코드
+		System.out.println("비번 변경전 : " + dto.getUserPwd());
+		String securePwd = psencoder.encode(dto.getUserPwd());
+		System.out.println("비번 변경 후 : " + securePwd);
+		
+		dto.setUserPwd(securePwd); //암호 변경
+		
 		int result = 0;
 		try {//동일 id로 가입시 프로그램은 계속되도록
 			result = mapper.signUp(dto);			
