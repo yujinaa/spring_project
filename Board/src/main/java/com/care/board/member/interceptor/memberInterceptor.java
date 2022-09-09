3d2affa76aa542d7b06de0f5f9a2fa8ef219c269
yujinaa <olim920@naver.com>
2022-09-10 01:52:05 +0900

    인터셉터-세션 없으면 alert창 띄우고 로그인 페이지로 이동하기

--- Board/src/main/java/com/care/board/member/interceptor/memberInterceptor.java
+++ Board/src/main/java/com/care/board/member/interceptor/memberInterceptor.java
@@ -1,5 +1,7 @@
 package com.care.board.member.interceptor;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -17,7 +19,11 @@ public class memberInterceptor extends HandlerInterceptorAdapter implements logi
 		HttpSession session = request.getSession(); //세션 얻어오기
 		
 		if(session.getAttribute(LOGIN)==null) {
-			response.sendRedirect("login"); //세션 없으면 바로 로그인 페이지 띄우기
+//			response.sendRedirect("login"); //세션 없으면 바로 로그인 페이지 띄우기
+			response.setContentType("text/html; charset = utf-8"); //세션없으면 alert메시지 띄운후 이동
+			PrintWriter out =response.getWriter();
+			out.print("<script>alert('로그인 해주세요'); "
+					+  "location.href ='"+request.getContextPath() +"/member/login';</script>"); 
 			return false; //false는 사용자가 요청한 경로로 가지 않고, true면 사용자가 요청한 경로로 연결
 		}
 		
