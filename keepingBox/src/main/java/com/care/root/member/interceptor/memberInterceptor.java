5dcb6d89b3b17cbdd310f2b188c54a1820ed9bbe
yujinaa <olim920@naver.com>
2022-10-07 13:17:53 +0900

    인터셉터완료

--- keepingBox/src/main/java/com/care/root/member/interceptor/memberInterceptor.java
+++ keepingBox/src/main/java/com/care/root/member/interceptor/memberInterceptor.java
@@ -1,7 +1,10 @@
 package com.care.root.member.interceptor;
 
+import java.io.PrintWriter;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -12,8 +15,18 @@ public class memberInterceptor extends HandlerInterceptorAdapter implements memb
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		System.out.println("인터셉터실행");
-		return true;
-	}
 
+		HttpSession session = request.getSession(); //세션 얻어오기
+
+		if(session.getAttribute(LOGIN) == null) {//여기서 미리 작성
+			//response.sendRedirect("login");
+			response.setContentType("text/html; charset=utf-8");
+			PrintWriter out = response.getWriter();
+			out.print("<script>alert('로그인 후 이용가능합니다.'); "
+					+ "location.href ='"+request.getContextPath() +"/member/login';</script>"); 
+			return false;  //해당 redirect
+		}
+		System.out.println("인터셉터실행");
+		return true;  //해당 사용자가 요청한 경로가 들어간다. 인터셉터가 끝나면 멤버컨트롤러가 실행
+	}
 }
