e1cd1a5d4aa6d76c275db401dfa2c365ba0350de
yujin Na <olim920@naver.com>
2023-01-31 15:59:39 +0900

    수정

--- shoppingmall_project/src/main/java/com/care/root/mybatis/member/MemberMapper.java
+++
@@ -1,9 +0,0 @@
-package com.care.root.mybatis.member;
-
-import com.care.root.member.dto.memberDTO;
-
-public interface MemberMapper {
-	public memberDTO userCheck(String id); //dto값 전달. 이 mapper파일은 xml 파일과 연결된다.
-	public int join(memberDTO dto);
-
-}
