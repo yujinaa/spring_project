23a0dbc093d475bc1d8abad2f91523783af0528e
yujin Na <olim920@naver.com>
2023-05-11 17:31:43 +0900

    결제 시스템 구현중

--- keepingBox/src/main/webapp/resources/js/bookingInfo.js
+++ keepingBox/src/main/webapp/resources/js/bookingInfo.js
@@ -4,10 +4,6 @@
 const cancelBtn = document.getElementById("cancel-btn");
 const mainBtn = document.getElementById("main-btn");
 
-const payBtn = document.getElementById("pay-btn");
-
-
-
 cancelBtn.addEventListener("click", () => {
 	if (confirm("예약을 취소하시겠습니까?")) {
 		alert("취소되었습니다.");
@@ -18,51 +14,3 @@ cancelBtn.addEventListener("click", () => {
 	}
 });
 
-
-	/* 
-var IMP = window.IMP;
-IMP.init("imp42434633");
-
-
-function requestPay() {
-	html5_inicis : 이니시스, kakaopay
-var bookInfo = '${ price }';
-var bookInfo = $('.intmoney').val();
-	
-
-	IMP.request_pay({
-		pg: 'html5_inicis',
-		pay_method: 'card',
-		merchant_uid: 'merchant_' + new Date().getTime(),
-
-		name: '주문명 : 주문명 설정',
-		amount: '<%=price%>',
-		buyer_email: '${ booking.email }',
-		buyer_name: '${ booking.name }',
-		buyer_tel: '${ booking.phone }',
-	}, function(rsp) {
-		console.log(rsp);
-		if (rsp.success) {
-			var msg = '결제가 완료되었습니다.';
-			msg += '고유ID : ' + rsp.imp_uid;
-			msg += '상점 거래ID : ' + rsp.merchant_uid;
-			msg += '결제 금액 : ' + rsp.paid_amount;
-			msg += '카드 승인번호 : ' + rsp.apply_num;
-			console.log("결제성공 " + msg);
-			$.ajax({
-				type: "GET",
-				url: "/booking/payment",
-				data: {
-					"amount": requestPay
-				},
-			});
-		} else {
-			var msg = '결제에 실패하였습니다.';
-			msg += '에러내용 : ' + rsp.error_msg;
-		}
-		alert(msg);
-		
-		document.location.href="index"; 
-	});
-}
-		*/
